d3e9e57679ddc94a172ac80ab680c4e42494f1a5
HP <HP@DESKTOP-BTQ2T4D.lan>
2023-01-26 12:38:24 +0530

    code written for webTextBox

--- commonFunctions/src/test/java/commonFunctions/WebTextBox.java
+++ commonFunctions/src/test/java/commonFunctions/WebTextBox.java
@@ -1,5 +1,37 @@
 package commonFunctions;
 
+import org.openqa.selenium.WebElement;
+
 public class WebTextBox {
+	
+	/*
+	 * purpose to find whether the element is present in the dom or not
+	 */
+	public static boolean isPresent(WebElement element) {
+		
+		boolean isPre = false;
+		if(element.isDisplayed()) {
+			isPre = true;
+		}
+		return isPre;
+	}
+	
+	/*
+	 *  checks whether element is clickable or not
+	 */
+	
+	public static boolean isClickable(WebElement element) {
+		
+		boolean isClick = false;	
+		if(isPresent(element)) {
+			
+			if(element.isEnabled()) {
+				isClick = true;
+			}
+		}
+		return isClick;
+	}
+	
 
 }
+
